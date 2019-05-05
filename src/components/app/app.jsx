90bd8892c0d008e6162325c01747c761d7fb622d
Max <intelmax3@gmail.com>
2019-05-05 13:21:08 +0300

    Конфликт №1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import MainScreen from '../main-screen/main-screen.jsx';
 
 const App = () => {
+  const filmsList = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`];
   const emptyClick = (evt) => {
     evt.preventDefault();
   };
 
   return <MainScreen
+    filmsList={filmsList}
     onClick = {emptyClick}
   />;
 };
